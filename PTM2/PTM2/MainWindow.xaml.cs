7e6687656bf7283e58fe2f03d0f0c503dfdc24df
Sabotage84 <sabotage84@mail.ru>
2020-09-29 23:25:26 +0300

    ADD server class

--- PTM2/PTM2/MainWindow.xaml.cs
+++ PTM2/PTM2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PTM2.equipment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace PTM2
         public MainWindow()
         {
             InitializeComponent();
+            
+            
         }
     }
 }
---
+++ PTM2/PTM2/equipment/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.equipment
+{
+    class Server:BaseEquipment
+    {
+        public Server(string sName, double coef = 2.6)
+        {
+            PreName = "Сервер точного времени";
+            K = coef;
+
+        }
+    }
+}
