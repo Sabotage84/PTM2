d1ae23d606419512f8c81842f5f76ad41b67a890
Sabotage84 <sabotage84@mail.ru>
2020-09-29 22:56:29 +0300

    ADD base equipment class

---
+++ PTM2/PTM2/equipment/BaseEquipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.equipment
+{
+    class BaseEquipment
+    {
+        string preName;
+
+        public string PreName { get => preName; set => preName = value; }
+        public string ShortName { get => shortName; set => shortName = value; }
+        public string FullName { 
+            get {
+                return PreName + " " + ShortName; 
+            } }
+
+        public double K { get => k; set => k = value; }
+        public double Price { get
+            {
+                return K * inPrise;
+            }
+            }
+
+        public string Description { get => description; set => description = value; }
+
+        string shortName;
+        string fullName;
+        double inPrise;
+        double k;
+        double price;
+        string description;
+
+
+    }
+}
