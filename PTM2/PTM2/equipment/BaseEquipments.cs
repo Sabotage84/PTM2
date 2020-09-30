0f6fd281d49d5316967b3535baf02efa4de77076
Sabotage84 <sabotage84@mail.ru>
2020-09-30 22:49:24 +0300

    ADD singltone and serialize to baseEquipments

--- PTM2/PTM2/equipment/BaseEquipments.cs
+++ PTM2/PTM2/equipment/BaseEquipments.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
 
 namespace PTM2.equipment
 {
+    [Serializable]
     class BaseEquipments: IEnumerable
     {
         List<BaseEquipment> eqList = new List<BaseEquipment>();
+
+        private static readonly Lazy<BaseEquipments> lazy =
+        new Lazy<BaseEquipments>(() => new BaseEquipments());
+
+        public static BaseEquipments modulInstance { get { return lazy.Value; } }
 
         public IEnumerator GetEnumerator()
         {
@@ -25,5 +34,50 @@ namespace PTM2.equipment
         {
             eqList.Remove(item);
         }
-    }
+
+        public void Save()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<BaseEquipment>));
+
+
+            using (FileStream fs = new FileStream("eq.xml", FileMode.OpenOrCreate))
+            {
+
+                try
+                {
+                    formatter.Serialize(fs,eqList);
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось создать файл прайса!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+
+        }
+
+        private BaseEquipments()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<BaseEquipment>));
+
+
+            using (FileStream fs = new FileStream("eq.xml", FileMode.OpenOrCreate))
+            {
+
+                try
+                {
+                    eqList = (List<BaseEquipment>)formatter.Deserialize(fs);
+                }
+                catch
+                {
+
+                    MessageBox.Show("Файл с првйсом не найден!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    formatter.Serialize(fs, eqList);
+                }
+
+            }
+        }
+
+
+        }
 }
