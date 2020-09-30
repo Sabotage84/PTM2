5dcbb5ec78efa87876d930282b569508f5a07bfa
Sabotage84 <sabotage84@mail.ru>
2020-09-30 22:25:50 +0300

    ADD eqs class not done and override equal Meth in baseEq

--- PTM2/PTM2/equipment/BaseEquipment.cs
+++ PTM2/PTM2/equipment/BaseEquipment.cs
@@ -32,7 +32,22 @@ namespace PTM2.equipment
         double k;
         double price;
         string description;
+        public override bool Equals(object obj)
+        {
+            if(obj.GetType() != GetType()) return false;
 
+            BaseEquipment item = (BaseEquipment)obj;
+            return (item.FullName==FullName);
+        }
 
+        public override int GetHashCode()
+        {
+            var hashCode = 1327208659;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(preName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(shortName);
+            hashCode = hashCode * -1521134295 + inPrise.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(description);
+            return hashCode;
+        }
     }
 }
---
+++ PTM2/PTM2/equipment/BaseEquipments.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.equipment
+{
+    class BaseEquipments: IEnumerable
+    {
+        List<BaseEquipment> eqList = new List<BaseEquipment>();
+
+        public IEnumerator GetEnumerator()
+        {
+            return eqList.GetEnumerator();
+        }
+
+        public void ADDItem(BaseEquipment item)
+        {
+            eqList.Add(item);
+        }
+
+        public void RemoveItem(BaseEquipment item)
+        {
+            eqList.Remove(item);
+        }
+    }
+}
