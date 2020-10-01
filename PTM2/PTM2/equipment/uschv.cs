80c8249629d270fd2f98721ea65e6e6c8f11b3d9
Sabotage84 <sabotage84@mail.ru>
2020-10-01 21:24:53 +0300

    ADD гысрм

---
+++ PTM2/PTM2/equipment/uschv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.equipment
+{
+    class uschv:BaseEquipment
+    {
+        public uschv(string sName, double coef = 2.6)
+        {
+            PreName = "Устройство синхронизации частоты и времени";
+            K = coef;
+
+        }
+    }
+}
