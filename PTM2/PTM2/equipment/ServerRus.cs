29cc03a0b429e98c3752528591bb452d384eecf8
Sabotage84 <sabotage84@mail.ru>
2020-10-05 18:13:01 +0300

    Add servsr rus

---
+++ PTM2/PTM2/equipment/ServerRus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.equipment
+{
+    class ServerRus:BaseEquipment
+    {
+
+        public ServerRus(string sName, double coef = 2)
+        {
+            PreName = "Сервер точного времени";
+            K = coef;
+        }
+    }
+}
