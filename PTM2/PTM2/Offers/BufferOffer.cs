9792820363113b3caece58213f9a6448b172859a
Sabotage84 <sabotage84@mail.ru>
2020-11-25 23:36:25 +0300

    ADD buffer offer

---
+++ PTM2/PTM2/Offers/BufferOffer.cs
@@ -0,0 +1,21 @@
+﻿using PTM2.equipment;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTM2.Offers
+{
+    class BufferOffer
+    {
+        public BufferOffer(ObservableCollection<BaseEquipment> offerList)
+        {
+
+        }
+        string bufferMessage;
+
+        public string BufferMessage { get => bufferMessage; set => bufferMessage = value; }
+    }
+}
