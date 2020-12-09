9387bbe6be8cb8b1710547c3bcd6b6c547ee700e
Sabotage84 <sabotage84@mail.ru>
2020-12-09 23:52:34 +0300

    ADD load type file

--- PTM2/PTM2/equipment/BaseEquipments.cs
+++ PTM2/PTM2/equipment/BaseEquipments.cs
@@ -15,14 +15,15 @@ using System.Xml.Serialization;
 namespace PTM2.equipment
 {
     [Serializable]
-    public class BaseEquipments: IEnumerable, INotifyPropertyChanged
+    public class BaseEquipments : IEnumerable, INotifyPropertyChanged
     {
         ObservableCollection<BaseEquipment> eqList = new ObservableCollection<BaseEquipment>();
         ObservableCollection<BaseEquipment> defaultList = new ObservableCollection<BaseEquipment>();
         ObservableCollection<BaseEquipment> offerList = new ObservableCollection<BaseEquipment>();
         string eqFilePath;
-        
-        
+        string typeFilePath;
+        Dictionary<string, string> typeOfEquipment = new Dictionary<string, string>();
+
         private static readonly Lazy<BaseEquipments> lazy =
         new Lazy<BaseEquipments>(() => new BaseEquipments());
 
@@ -30,16 +31,19 @@ namespace PTM2.equipment
 
         public static BaseEquipments modulInstance { get { return lazy.Value; } }
 
-        public ObservableCollection<BaseEquipment> EqList { get => eqList; 
+        public ObservableCollection<BaseEquipment> EqList
+        {
+            get => eqList;
             set
-                {
-                    eqList = value;
-                }
+            {
+                eqList = value;
+            }
         }
         public ObservableCollection<BaseEquipment> DefaultList { get => defaultList; set => defaultList = value; }
         public ObservableCollection<BaseEquipment> OfferList { get => offerList; set => offerList = value; }
         public string EqFilePath { get => eqFilePath; set => eqFilePath = value; }
-        
+        public Dictionary<string, string> TypeOfEquipment { get => typeOfEquipment; set => typeOfEquipment = value; }
+        public string TypeFilePath { get => typeFilePath; set => typeFilePath = value; }
 
         public IEnumerator GetEnumerator()
         {
@@ -50,8 +54,6 @@ namespace PTM2.equipment
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-
 
         public void ADDItem(BaseEquipment item)
         {
@@ -79,26 +81,68 @@ namespace PTM2.equipment
             eqList.CollectionChanged += eqList_CollectionChanged;
             offerList.CollectionChanged += offerList_CollectionChanged;
             EqFilePath = @"../../eq.xml";
+            TypeFilePath = "type.txt";
             XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<BaseEquipment>));
 
             using (FileStream fs = new FileStream(EqFilePath, FileMode.OpenOrCreate))
             {
-
                 try
                 {
-                   
                     EqList = (ObservableCollection<BaseEquipment>)formatter.Deserialize(fs);
                     DefaultList = new ObservableCollection<BaseEquipment>(EqList);
+                    GetTypeList(TypeFilePath);
                     //DefaultList = (ObservableCollection<BaseEquipment>)formatter.Deserialize(fs);
                 }
                 catch
                 {
                     string tmp = fs.Name;
-                    MessageBox.Show("Файл с прайсом не найден!\n"+tmp, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    
+                    MessageBox.Show("Файл с прайсом не найден!\n" + tmp, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
                 }
             }
         }
+
+        private void GetTypeList(string v)
+        {
+            try
+            {
+                string[] arStr = File.ReadAllLines(v);
+                if (arStr.Length>2)
+                {
+                    for (int i = 0; i < arStr.Length; i+=2)
+                    {
+                        TypeOfEquipment.Add(arStr[i], arStr[i + 1]);
+                    }
+                }
+                else
+                {
+                    TypeOfEquipment.Add("STV", "Сервер");
+                    TypeOfEquipment.Add("USCHV", "Устройство");
+                    TypeOfEquipment.Add("CON", "Преобразователь");
+                    TypeOfEquipment.Add("SDU", "Размножитель");
+                    TypeOfEquipment.Add("ANT", "Антенна");
+                    TypeOfEquipment.Add("CAB", "Кабель");
+                    TypeOfEquipment.Add("PROT", "Грозозащита");
+                    TypeOfEquipment.Add("CLOCK", "Часы");
+                    string tmp = "";
+                    foreach (var item in TypeOfEquipment)
+                    {
+                        tmp += item.Key + "\n" + item.Value + "\n";
+                    }
+                    tmp = tmp.Substring(0, tmp.Length - 1);
+                    File.WriteAllText(v, tmp);
+                }
+
+            }
+            catch
+            {
+
+                MessageBox.Show("Загрузка типов неудалась!\n", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+        }
+
         public void SevePriceList()
         {
             XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<BaseEquipment>));
@@ -142,7 +186,7 @@ namespace PTM2.equipment
         }
         public void SearchEqByName(string searchString)
         {
-            if(string.IsNullOrEmpty(searchString))
+            if (string.IsNullOrEmpty(searchString))
             {
                 EqList.Clear();
                 foreach (var item in DefaultList)
